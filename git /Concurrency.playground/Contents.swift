06a80bf0c82706aac08d7824321931937b67068e
Tenzin Wangyal <tenzinwangyal20@yahoo.com>
2022-06-11 15:45:51 -0500

    new files and documentation

---
+++ git /Concurrency.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+import _Concurrency
+actor TemperatureLogger {
+    let label: String
+    var measurements: [Int]
+    private(set) var max: Int
+
+    init(label: String, measurement: Int) {
+        self.label = label
+        self.measurements = [measurement]
+        self.max = measurement
+    }
+}
+
+let logger = TemperatureLogger(label: "Outdoors", measurement: 50)
+Task {
+    print(await logger.max)
+    // Prints "25"
+}
+
+extension TemperatureLogger {
+    func update(with measurement: Int) {
+        measurements.append(measurement)
+        if measurement > max {
+            max = measurement
+        }
+    }
+}
+
